8d45d0ced6f41375cd90ea14b73caa515a5a9290
Denys Korniienko <denys.korniienko@gamanet.com>
2020-07-21 11:57:06 +0300

    Services Middleware

---
+++ Services.Middleware/Middleware/TimerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Services.CreateOwnServices.services;
+using System.Threading.Tasks;
+
+namespace Services.Middleware.Middleware
+{
+    public class TimerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private TimeService _timeService;
+
+        public TimerMiddleware(RequestDelegate next, TimeService timeService)
+        {
+            _next = next;
+
+            // more usable for singleton services
+            _timeService = timeService;
+        }
+
+        public async Task InvokeAsync(HttpContext context, TimeService timeService)
+        {
+            if (context.Request.Path.Value.ToLower() == "/time")
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                // await context.Response.WriteAsync($"{_timeService.Time}");
+                await context.Response.WriteAsync($"{timeService.Time}");
+
+                // depended on environment 
+                //var srv = context.RequestServices.GetService(typeof(TimeService)) as TimeService;
+                //await context.Response.WriteAsync(srv?.Time);
+            }
+            else
+            {
+                _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ Services.Middleware/Services/TimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.CreateOwnServices.services
+{
+    public class TimeService
+    {
+        public string Time { get; }
+
+        public TimeService()
+        {
+            Time = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
